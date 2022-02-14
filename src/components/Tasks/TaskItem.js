a7794cd0b4d26ae0346b1b516f15a105cd4d45c6
Aman mool <amanmool384@gmail.com>
2022-02-14 11:12:42 +0545

    Completed 15.2

--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -1,7 +1,7 @@
-import classes from './TaskItem.module.css';
-
+import classes from "./TaskItem.module.css";
+import React from "react";
 const TaskItem = (props) => {
-  return <li className={classes.task}>{props.children}</li>
+  return <li className={classes.task}>{props.children}</li>;
 };
 
 export default TaskItem;
--- src/components/UI/Section.js
+++ src/components/UI/Section.js
@@ -1,4 +1,5 @@
-import classes from './Section.module.css';
+import classes from "./Section.module.css";
+import React from "react";
 
 const Section = (props) => {
   return <section className={classes.section}>{props.children}</section>;
