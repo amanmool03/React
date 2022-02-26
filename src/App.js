5f43e38c19830a9858803ad335ef4fe909f82c76
Aman mool <amanmool384@gmail.com>
2022-02-26 20:15:58 +0545

    remains

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
-import React, { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import "./components/Expenses/Expenses.css";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { Component } from "react";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet paper1",
-    amount: 299,
-    date: new Date(2020, 7, 14),
-  },
-];
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
-export default function App() {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = (expense) => {
-    // setExpenses([expense, ...expenses]);
-    // here depended on the previous state so instead of above use function
-
-    setExpenses((prevExpense) => {
-      return [expense, ...prevExpense];
-    });
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
   };
 
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
-    </div>
-  );
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal closed={this.closeModal} show={state} />}
+        </Transition>
+
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
 }
+
+export default App;
